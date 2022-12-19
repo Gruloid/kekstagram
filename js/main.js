3d1c04c1014cc3444c067f706ffe96a42a20cd10
gruloid_home <gruloid@gmail.com>
2022-12-19 23:07:47 +0300

    add js from mac and init

--- js/main.js
+++ js/main.js
@@ -1,14 +1,53 @@
-function randomizer(min, max) {
-  if (min < 0 || max < 0) {
-    return -1;
-  }
-  if (min > max) {
-    [min, max] = [max, min];
-  }
-  return (Math.floor(Math.random() * (max - min + 1)) + min);
-}
-
 function checkCommit(str, value = 140) {
   return str.length <= value;
 }
+
+function getRandomNum(a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+}
+const PHOTO_DESCRIPTION = [
+  'Передо мной интересная фотография.',
+  'Давайте рассмотрим изображение внимательнее',
+  'Мне нравится эта фотография, потому что она точно передаёт настроение',
+  'На переднем плане изображены…',
+  'Если внимательно посмотреть на изображение, то…',
+  'Композиция снимка весьма интересна, потому что…',
+  'Выражения лиц у героев фотографии говорят о том, что…',
+  'Фото несёт в себе определённое настроение:…',
+];
+const TEXT_MESSAGE = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец     из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+let createComments = () => {
+  return {
+    id: getRandomNum(0, 100),
+    avatar: 'img/avatar-' + getRandomNum(1, 6) + '.svg',
+    message: TEXT_MESSAGE[getRandomNum(0, TEXT_MESSAGE.length - 1)],
+    name: '',
+  }
+};
+
+let idPhoto = 0;
+let createPhoto = () => {
+  idPhoto++;
+  return {
+    id: idPhoto,
+    url: 'photos/' + idPhoto + '.jpg',
+    description: PHOTO_DESCRIPTION[getRandomNum(0, PHOTO_DESCRIPTION.length - 1)],
+    likes: getRandomNum(15, 200),
+    comments: Array.from({ length: getRandomNum(1, 5) }, createComments),
+  }
+};
+
+let photoArray = Array.from({ length: 25 }, createPhoto)
+
+console.log(photoArray);
 
