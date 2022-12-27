6385ca6fc2ce0058257f9fa1ec6bc81b268be69a
gruloid_home <gruloid@gmail.com>
2022-12-27 20:32:49 +0300

    modulu 4

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-function checkCommit(str, value = 140) {
-  return str.length <= value;
-}
+// function checkCommit(str, value = 140) {
+//   return str.length <= value;
+// }
 
 function getRandomNum(a, b) {
   const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
@@ -8,6 +8,26 @@ function getRandomNum(a, b) {
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
 }
+const NAMES = [
+  'Емелиан',
+  'Лукьян',
+  'Ефим',
+  'Лучезар',
+  'Назар',
+  'Ной',
+  'Платон',
+  'Потап',
+  'Радомир',
+  'Трофим',
+  'Агата',
+  'Агафья',
+  'Василина',
+  'Евдокия',
+  'Лада',
+  'Лукерья',
+  'Серафима',
+  'Сияна',
+];
 const PHOTO_DESCRIPTION = [
   'Передо мной интересная фотография.',
   'Давайте рассмотрим изображение внимательнее',
@@ -21,22 +41,20 @@ const PHOTO_DESCRIPTION = [
 const TEXT_MESSAGE = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец     из кадра. В конце концов это просто непрофессионально.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
 ];
-let createComments = () => {
-  return {
-    id: getRandomNum(0, 100),
-    avatar: 'img/avatar-' + getRandomNum(1, 6) + '.svg',
-    message: TEXT_MESSAGE[getRandomNum(0, TEXT_MESSAGE.length - 1)],
-    name: '',
-  }
-};
+const createComments = () => ({
+  id: getRandomNum(0, 100),
+  avatar: 'img/avatar-' + getRandomNum(1, 6) + '.svg',
+  message: TEXT_MESSAGE[getRandomNum(0, TEXT_MESSAGE.length - 1)],
+  name: NAMES[getRandomNum(0, NAMES.length - 1)],
+});
 
 let idPhoto = 0;
-let createPhoto = () => {
+const createPhoto = () => {
   idPhoto++;
   return {
     id: idPhoto,
@@ -44,10 +62,10 @@ let createPhoto = () => {
     description: PHOTO_DESCRIPTION[getRandomNum(0, PHOTO_DESCRIPTION.length - 1)],
     likes: getRandomNum(15, 200),
     comments: Array.from({ length: getRandomNum(1, 5) }, createComments),
-  }
+  };
 };
 
-let photoArray = Array.from({ length: 25 }, createPhoto)
-
-console.log(photoArray);
+const getPhotoArray = () => Array.from({ length: 25 }, createPhoto);
+//getPhotoArray();
+//console.log(photoArray);
 
